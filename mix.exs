bb46d80170d97a2101fe0da1de4c0979e6ffd382
David Bernheisel <david@bernheisel.com>
2026-02-24 21:07:32 -0500

    Bump release

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Phantom.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
       start_permanent: Mix.env() == :prod,
-      version: "0.3.3",
+      version: "0.3.4",
       source_url: "https://github.com/dbernheisel/phantom_mcp"
     ]
   end
