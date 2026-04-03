241e084166f54e0d2369e123292047016b41992c
David Bernheisel <david@bernheisel.com>
2026-04-02 20:28:06 -0400

    feat: spawn multi-node cluster in test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,10 @@
+exclude = Keyword.get(ExUnit.configuration(), :exclude, [])
+
+unless :clustered in exclude do
+  Phantom.Test.Cluster.spawn([
+    {:"node1@127.0.0.1", port: 4101},
+    {:"node2@127.0.0.1", port: 4102}
+  ])
+end
+
 ExUnit.start()
